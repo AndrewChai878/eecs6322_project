8750ddf53e42e5f3f41e1a661483a077ae23a692
Alex Bianchi <55216478+alexBianchi4@users.noreply.github.com>
2024-03-28 18:58:13 -0400

    Modified network to work with both R strategies and any R setting (Images)

--- coordx.py
+++ coordx.py
@@ -16,28 +16,36 @@ def siren_init(module:nn.Module):
             w.uniform_(-bound, bound)
 
 class coordx_net(nn.Module):
-    def __init__(self, n_branches:int, out_channels:int, hidden_size:int, initialize:bool=True):
+    def __init__(self, n_branches:int, out_channels:int, hidden_size:int, R:int=1, R_strat:int=1, initialize:bool=True):
         '''
         CoordX network as described in the paper: https://arxiv.org/pdf/2201.12425.pdf
         @param branches: number of branches in the network (2 for image, 3 for video)
         @param out_channels: number of output channels (3 for RGB, 1 for grayscale)
         @param hidden_size: hidden size of the network (256 for image, 1024 for video)
+        @param R: scale by which to increase hidden feature size (increases image quality; set = 2 for image)
+        @param R_strat: strategy for increasing hidden feature size. 
+            1 --> increase just the layer before the merge (recommended); 2 --> increase all layers before merge
         @param intialize: whether to initialize the network with Siren initialization
                             - not included in the paper but recommended
         '''
         super(coordx_net, self).__init__()
+        assert n_branches in [2,3], "Number of branches must be 2 or 3"
+        assert type(R) is int, "R must be an integer"
         self.n_branches = n_branches
         self.channels = out_channels
+        self.R = R
+        # the scaling factor for the hidden layers before the merge (except the layer immediately before it)
+        R_all = 1 if R_strat == 1 else R
         # the input branches
         self.branches = nn.ModuleList()
         for _ in range(n_branches):
-            self.branches.append(nn.Linear(1,hidden_size))
+            self.branches.append(nn.Linear(1,hidden_size * R_all))
         # the shared hidden layers (premerge)
         self.premerge = nn.Sequential(
             Sine(w0=1.0),
-            nn.Linear(hidden_size,hidden_size),
+            nn.Linear(hidden_size * R_all, hidden_size * R_all),
             Sine(w0=1.0),
-            nn.Linear(hidden_size,hidden_size),
+            nn.Linear(hidden_size * R_all, hidden_size * R),
             Sine(w0=1.0)
         )
         # the shared hidden layers (postmerge)
@@ -47,7 +55,7 @@ class coordx_net(nn.Module):
             nn.Linear(hidden_size,self.channels),
             nn.Sigmoid()
         )
-    
+        # initialize the network with Siren initialization
         if initialize:
             for module in self.branches.modules():
                 siren_init(module)            
@@ -60,11 +68,14 @@ class coordx_net(nn.Module):
         '''
         implementation of the fusion operator in the paper
         @param dims: the input tensors --> [x,y] for image; [x,y,t] for video
-        '''        
-        if self.n_branches == 2:            
-            return torch.einsum("ik,jk->ijk",dims[0],dims[1])
+        '''
+        if self.n_branches == 2:        
+            if self.R > 1:
+                dims[0] = torch.einsum("ijk->ik", dims[0])
+                dims[1] = torch.einsum("ijk->ik", dims[1])
+            return torch.einsum("ik,jk->ijk", dims[0], dims[1])
         elif self.n_branches == 3:
-            return torch.einsum("ih,jh,kh->ijkh",dims[0],dims[1],dims[2])
+            return torch.einsum("ih,jh,kh->ijkh", dims[0], dims[1], dims[2])
     
     def reshape(self, tensor:torch.tensor) -> torch.tensor:
         '''
@@ -76,7 +87,16 @@ class coordx_net(nn.Module):
             return tensor.permute(2,1,0)
         elif self.n_branches == 3:
             # reshape to (T, H, W, C)
-            return tensor.permute(2,1,0,3)            
+            return tensor.permute(2,1,0,3)
+    
+    def map_to_h(self, x:torch.tensor) -> torch.tensor:
+        ''' 
+        reshapes the input tensor so that its dimensionality is in line with the postmerge hidden layers
+        @param x: the input tensor (probably the output of the final premerge layer)
+        '''
+        if self.R > 1:
+            return torch.reshape(x, (x.shape[0], self.R, int(x.shape[1]/self.R)))
+        return x
 
     def forward(self, dims:list[torch.tensor]) -> torch.tensor:
         '''
@@ -88,8 +108,9 @@ class coordx_net(nn.Module):
         for i, dim in enumerate(dims):            
             out = self.branches[i](dim)
             out = self.premerge(out)
+            out = self.map_to_h(out)
             outputs.append(out)            
         merged = self.merge(outputs)        
         res = self.postmerge(merged)        
-        res = self.reshape(res)      
+        res = self.reshape(res)
         return res
